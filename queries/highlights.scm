65deff2300617bfd6d5c2189f73bf6366fbd0001
Teddytrombone <github@tmk-stgeorgen.at>
2023-10-20 08:00:07 +0200

    optimized highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,19 +1,28 @@
-(identifier) @type
+(identifier) @property
 
 (constant) @constant
 
 (modifier_function) @function
-(condition) @function.condition
-(condition_end) @function.condition
-(condition_else) @function.condition
+(modifier_predefined) @function.builtin
+
+[
+  (condition) 
+  (condition_end) 
+  (condition_else)
+] @conditional
 
 
 [
   "@import"
   "INCLUDE_TYPOSCRIPT"
-  (modifier_predefined)
-  (cobject)
 ] @keyword
+
+(cobject) @type.builtin
+
+[
+  "@import"
+  "INCLUDE_TYPOSCRIPT"
+] @include
 
 [
   (comment)
